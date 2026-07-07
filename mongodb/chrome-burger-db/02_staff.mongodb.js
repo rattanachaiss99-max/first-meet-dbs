362dbeb4592912152e176e901db38148fa8220f8
Rattanachai Kanjiak <rattanachaiss99@gmail.com>
2026-07-07 12:00:35 +0700

    burger-db commit

---
+++ mongodb/chrome-burger-db/02_staff.mongodb.js
@@ -0,0 +1,34 @@
+use("chrome-burger-db")
+
+//cleardata
+db.staff.deleteMany({});
+
+db.staff.insertMany([
+    {
+        "_id": ObjectId("65f100000000000000000001"),
+        "first_name": "Jane",
+        "last_name": "Doe",
+        "role": "Cashier"
+    },
+    {
+        "_id": ObjectId("65f100000000000000000002"),
+        "first_name": "John",
+        "last_name": "Smith",
+        "role": "Cook"
+    },
+    {
+        "_id": ObjectId("65f100000000000000000003"),
+        "first_name": "Emily",
+        "last_name": "Jones",
+        "role": "Cashier"
+    },
+    {
+        "_id": ObjectId("65f100000000000000000004"),
+        "first_name": "Chris",
+        "last_name": "Williams",
+        "role": "Cook"
+    }
+]);
+
+//Find
+db.staff.find({});
